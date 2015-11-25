bb6ca56b4f72d9f481cc87e2640dab0f7c760f1c
Gustavo Mendonca <gustavogmend@gmail.com>
2015-11-25 16:19:31 -0600

    better version

--- mongodb/bench.py
+++ mongodb/bench.py
@@ -8,11 +8,11 @@ import sys
 def string_generator(size=10, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
-client = MongoClient("localhost", 27017)
+client = MongoClient("172.31.32.209", 27017)
 
 db = client.database
 
-db.key_pair.create_index("key")
+#db.key_pair.create_index("key")
 db.key_pair.create_index("value")
 
 operations = 1000
@@ -24,14 +24,14 @@ print "Number of operations:", operations
 print "Starting timer..."
 startTotal = start = time.time()
 for i in range(operations):
-    db.key_pair.insert_one({"key": string_generator(10), "value": string_generator(90)})
+    db.key_pair.insert_one({"_id": string_generator(10), "value": string_generator(90)})
 print "Insert Time:",time.time() - start,"seconds"
 start = time.time()
 for i in range(operations):
-    db.key_pair.find_one({"key": string_generator(10)})
+    db.key_pair.find_one({"_id": string_generator(10)})
 print "Lookup Time:",time.time() - start,"seconds"
 start = time.time()
 for item in db.key_pair.find():
-    db.key_pair.remove({"key": item["key"]})
+    db.key_pair.remove({"_id": item["_id"]})
 print "Delete Time:",time.time() - start,"seconds"
 print "Overall Time:",time.time() - startTotal,"seconds"
