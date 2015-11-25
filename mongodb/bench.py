a41da686edea9e5d26eb71ed3341aa80a0a8ac15
Gustavo Mendonca <gustavogmend@gmail.com>
2015-11-25 10:32:11 -0600

    fixing timer

--- mongodb/bench.py
+++ mongodb/bench.py
@@ -18,17 +18,15 @@ operations = 1000
 
 
 print "Starting timer..."
-startTotal = timeit.default_timer()
-start_time = timeit.default_timer()
+startTotal = start = time.time()
 for i in range(operations):
     db.key_pair.insert_one({"key": string_generator(10), "value": string_generator(90)})
-start = time.time()
 print "Insert Time: ",time.time() - start
-print "Insert Time: ",timeit.default_timer() - start_time
+start = time.time()
 for i in range(operations):
     db.key_pair.find_one({"key": string_generator(10)})
-start = time.time()
 print "Lookup Time: ",time.time() - start
+start = time.time()
 for item in db.key_pair.find():
     db.key_pair.remove({"key": item["key"]})
 print "Delete Time: ",time.time() - start
