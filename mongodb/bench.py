6c3dbe7a5457f5a047203be8db9b72ef9dac624b
Gustavo Mendonca <gustavogmend@gmail.com>
2015-11-25 11:53:51 -0600

    everything working

--- mongodb/bench.py
+++ mongodb/bench.py
@@ -3,6 +3,7 @@ import string
 import random
 import time
 import timeit
+import sys
 
 def string_generator(size=10, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -15,19 +16,20 @@ db.key_pair.create_index("key")
 db.key_pair.create_index("value")
 
 operations = 1000
+print "Number of operations:", operations
 
 
 print "Starting timer..."
 startTotal = start = time.time()
 for i in range(operations):
     db.key_pair.insert_one({"key": string_generator(10), "value": string_generator(90)})
-print "Insert Time: ",time.time() - start
+print "Insert Time:",time.time() - start
 start = time.time()
 for i in range(operations):
     db.key_pair.find_one({"key": string_generator(10)})
-print "Lookup Time: ",time.time() - start
+print "Lookup Time:",time.time() - start
 start = time.time()
 for item in db.key_pair.find():
     db.key_pair.remove({"key": item["key"]})
-print "Delete Time: ",time.time() - start
-print "Overall Time: ",time.time() - startTotal
+print "Delete Time:",time.time() - start
+print "Overall Time:",time.time() - startTotal
